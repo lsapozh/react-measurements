c3b06b91f1e8ba22e28b2bc69aceeee96a5938a2
liliiasapozhnikova <sapozhnikova111@gmail.com>
2018-01-14 03:31:30 +0400

    8===э 🐱

--- src/components/MeasurementChart/index.js
+++ src/components/MeasurementChart/index.js
@@ -1,69 +1,59 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts';
-import {Message, ResponsiveContainerWrapper} from "./components/index";
+import {ResponsiveContainerWrapper} from "./components/index";
 
 export default class MeasurementChart extends Component {
-  formatDate = (d) => {
-    return d ? `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}` : '';
-  };
+    formatDate = (d) => {
+        return d ? `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}` : '';
+    };
 
-  // componentDidCatch(error, info) {
-  //       console.log(error, info);
-  //   }
-
-  render() {
-    const { measurement, records } = this.props;
-    const sortedRecords = [...records.filter((r) => r[measurement])];
-    sortedRecords.sort((r1, r2) => r1.date.getTime() - r2.date.getTime());
-    const finalRecords = [];
-    for (let i = 0; i < sortedRecords.length - 1; i += 1) {
-      let thisRecord = sortedRecords[i];
-      const nextRecord = sortedRecords[i + 1];
-      finalRecords.push(thisRecord);
-      while (this.formatDate(thisRecord.date) !== this.formatDate(nextRecord.date)) {
-        const nextDate = new Date(thisRecord.date.getTime());
-        nextDate.setDate(nextDate.getDate() + 1);
-        thisRecord = {
-          ...thisRecord,
-          date: nextDate,
-        };
-        if (this.formatDate(thisRecord.date) !== this.formatDate(nextRecord.date)) {
-          finalRecords.push(thisRecord);
+    render() {
+        const {measurement, records} = this.props;
+        const sortedRecords = [...records.filter((r) => r[measurement])];
+        sortedRecords.sort((r1, r2) => r1.date.getTime() - r2.date.getTime());
+        const finalRecords = [];
+        for (let i = 0; i < sortedRecords.length - 1; i += 1) {
+            let thisRecord = sortedRecords[i];
+            const nextRecord = sortedRecords[i + 1];
+            finalRecords.push(thisRecord);
+            while (this.formatDate(thisRecord.date) !== this.formatDate(nextRecord.date)) {
+                const nextDate = new Date(thisRecord.date.getTime());
+                nextDate.setDate(nextDate.getDate() + 1);
+                thisRecord = {
+                    ...thisRecord,
+                    date: nextDate,
+                };
+                if (this.formatDate(thisRecord.date) !== this.formatDate(nextRecord.date)) {
+                    finalRecords.push(thisRecord);
+                }
+            }
         }
-      }
-    }
-    finalRecords.push(sortedRecords[sortedRecords.length - 1]);
-    let data;
-    try {
-         data = finalRecords.map((record) => {
+        if (sortedRecords.length > 0) {
+            finalRecords.push(sortedRecords[sortedRecords.length - 1]);
+        }
+        let data = finalRecords.map((record) => {
             return {
                 xValue: this.formatDate(record.date),
                 yValue: record[measurement],
             };
         });
-    } catch (e) {
-      return (
-          <Message>
-              no data
-          </Message>
-      )
-    }
 
-    return (
-        <ResponsiveContainerWrapper>
-          <ResponsiveContainer width="100%" height="100%">
-              <AreaChart
-                  width={800} height={400} data={data}
-                  margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-              >
-                  <XAxis dataKey="xValue" />
-                  <YAxis domain={[0, 'dataMax + 50']} />
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <Tooltip />
-                  <Area type='linear' dataKey='yValue' stroke='rgba(0, 139, 139, 0.9)' fill='rgba(0, 139, 139, 0.9)' />
-              </AreaChart>
-          </ResponsiveContainer>
-      </ResponsiveContainerWrapper>
-    );
-  }
+        return (
+            <ResponsiveContainerWrapper>
+                <ResponsiveContainer width="100%" height="100%">
+                    <AreaChart
+                        width={800} height={400} data={data}
+                        margin={{top: 10, right: 30, left: 0, bottom: 0}}
+                    >
+                        <XAxis dataKey="xValue"/>
+                        <YAxis domain={[0, 'dataMax + 50']}/>
+                        <CartesianGrid strokeDasharray="3 3"/>
+                        <Tooltip/>
+                        <Area type='linear' dataKey='yValue' stroke='rgba(0, 139, 139, 0.9)'
+                              fill='rgba(0, 139, 139, 0.9)'/>
+                    </AreaChart>
+                </ResponsiveContainer>
+            </ResponsiveContainerWrapper>
+        );
+    }
 }
