26d061595e7650ce626a8d7ecb0c11db46d347ce
liliiasapozhnikova <sapozhnikova111@gmail.com>
2018-01-17 03:43:44 +0400

    added choosing measurements

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ const EditButton = styled.button`
     margin: 15px 15px 10px 0;
     padding: 5px 0 5px 0;
     font-size: 14px;
-    background-color: rgba(0, 139, 139, 1);
+    background-color: #008b8bad;
     // color: white;
     color: rgba(0, 0, 0, 0.8);
 `;
@@ -361,8 +361,8 @@ class App extends Component {
             <div className="app">
                 <ShowMeasurementsButton onClick={this.showMeasurementTypes}>
                     {/*<i class="fa fa-sort-desc" aria-hidden="true"></i>*/}
-                    <i className="fa fa-list" aria-hidden="true"></i>
-                    {/*<i class="fa fa-chevron-down" aria-hidden="true"></i>*/}
+                    {/*<i className="fa fa-list" aria-hidden="true"></i>*/}
+                    <i class="fa fa-chevron-down" aria-hidden="true"></i>
                 </ShowMeasurementsButton>
                 <MeasurementTypesDiv>
                     {MEASUREMENT_TYPES.map((type, index) => {
